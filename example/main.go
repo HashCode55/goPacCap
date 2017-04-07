3e5e8ef28ba8e0c4b3e55ceea1101914b5e2ea67
Mehul Ahuja <blackdragon0909@gmail.com>
2017-04-08 00:32:15 +0530

    src/dest logging and cache added

--- example/main.go
+++ example/main.go
@@ -2,11 +2,12 @@ package main
 
 // TODO: No absolute imports
 import (
-	".."
 	"fmt"
+	"github.com/hashcode55/gopaccap"
 )
 
 func main() {
 	fmt.Println("worked")
-	gopaccap.LiveCapture("tcp", "en0")
+	pc := gopaccap.PacketCapture()
+	pc.LiveCapture("tcp", "en0")
 }
