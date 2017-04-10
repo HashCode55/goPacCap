2ebdcc19cb2845c930255f49f1b579ef71b8378e
hashcode55 <blackdragon0909@gmail.com>
2017-04-11 01:10:25 +0530

    refactoring/packet struct for encapsulation

--- packetcapture.go
+++ packetcapture.go
@@ -3,6 +3,7 @@
 package gopaccap
 
 import (
+	"errors"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"time"
@@ -15,33 +16,33 @@ import (
 /////////////////////////
 // Global Declarations //
 /////////////////////////
-var banner string = `                                               
-$$$$$$$\                      $$$$$$\                      
-$$  __$$\                    $$  __$$\                     
-$$ |  $$ |$$$$$$\   $$$$$$$\ $$ /  \__| $$$$$$\   $$$$$$\  
-$$$$$$$  |\____$$\ $$  _____|$$ |       \____$$\ $$  __$$\ 
-$$  ____/ $$$$$$$ |$$ /      $$ |       $$$$$$$ |$$ /  $$ |
-$$ |     $$  __$$ |$$ |      $$ |  $$\ $$  __$$ |$$ |  $$ |
-$$ |     \$$$$$$$ |\$$$$$$$\ \$$$$$$  |\$$$$$$$ |$$$$$$$  |
-\__|      \_______| \_______| \______/  \_______|$$  ____/ 
-                                                 $$ |      
-                                                 $$ |      
-                                                 \__|      
-`
-
-var (
-	snapshot_len int32         = 65535
-	promiscuous  bool          = false
-	timeout      time.Duration = -1 * time.Second
-)
 
 var logger = log.New()
 
+// Packet stores the details of packets storing information about source
+// IP/Port and destination IP/Port
+type Packet struct {
+	// FromIP is the Source IP
+	FromIP string
+	// ToIP is the Destination IP
+	ToIP string
+	// FromPort is the Source Port
+	FromPort string
+	// ToPort is the Destination Port
+	ToPort string
+}
+
+// Pretty printing the packet
+func (p Packet) String() string {
+	return fmt.Sprintf("From %s:%s, To %s:%s}", p.FromIP, p.ToIP, p.FromPort, p.ToPort)
+}
+
 // Paccap is the enclosing packet capture struct. It encapsulates a
-// *IPCache object and a flag for logging the cache.
+// *IPCache object, and a packet channel which stores the
+// incoming packets
 type Paccap struct {
-	ipcache *IPCache // cache object
-	lc      bool     // flag for caching the log. Ignored while reading pcap files.
+	IPCache  *IPCache // cache object
+	PackChan chan Packet
 }
 
 /////////////////////////
@@ -49,72 +50,85 @@ type Paccap struct {
 /////////////////////////
 
 // PacketCapture creates a new instance of the Paccap
-// struct initialising the ipcache.
+// struct initialising the IPCache.
 // It takes a two arguments, cache expiration time and
 // a bool value specifying whether to log cache hits or not.
-func PacketCapture(exptime int, logcache bool) *Paccap {
-	// create a new ipcache with et as expiration time
+func PacketCapture(exptime int) *Paccap {
+	// create a new IPCache with et as expiration time
 	et := time.Duration(exptime)
 	// tick interval after which the entries are deleted
 	ti := time.Duration(exptime + 1)
 	ipc := NewIPCache(et*time.Minute, ti*time.Minute)
+
+	// create a new channel
+	packchan := make(chan Packet)
+
 	// new instance of Paccap
-	pc := &Paccap{ipcache: ipc, lc: logcache}
+	pc := &Paccap{IPCache: ipc, PackChan: packchan}
 	return pc
 }
 
 // ReadPcap reads the pcap files from the specified path and
 // logs the packet details. It takes two arguments, a packet filter
-// specified as BPF, and the path of the pcap files.
-func (pc *Paccap) ReadPcap(filter, path string) []string {
+// specified as BPF, and the path of the pcap files. As it recognizes the packets
+// it keeps on pushing the packets into a channel shared with the host process that is the
+// client.
+func (pc *Paccap) ReadPcap(filter, path string) {
 	logger.Infof("[PacCap ] Starting to read from pcap file...")
-	fmt.Println()
 	packetSource, err := readPackets(filter, path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var packetdetails []string
-
 	for packet := range packetSource.Packets() {
-		sip, dip := getIPAddresses(packet)
-		spn, dpn := getPortAddresses(packet)
-		logger.Infof("[PacCap ] Packet Details -- SOURCE %v:%v | DESTINATION %v:%v",
-			sip, spn, dip, dpn)
+		sip, dip, err := getIPAddresses(packet)
+		// if not recognized, log
+		if err != nil {
+			log.Errorf("[paccap ] %s", err.Error())
+		}
+		spn, dpn, err := getPortAddresses(packet)
+		// log if not recognized
+		if err != nil {
+			log.Errorf("[paccap ] %s", err.Error())
+		}
 
-		s := fmt.Sprintf("src %s:%s | dst %s:%s", sip, spn, dip, dpn)
-		packetdetails = append(packetdetails, s)
-		fmt.Println()
+		pkt := Packet{FromIP: sip, ToIP: dip, FromPort: spn, ToPort: dpn}
+		pc.PackChan <- pkt
+
+		// populating the cache
+		pc.IPCache.Set(pkt)
 	}
-	return packetdetails
 }
 
 // LiveCapture attaches with the NIC specified and starts capturing
 // the packets logging the packet details. It caches the source IP of the
-// packets recieved with the given expiration time. Takes BPF(filter) and
-// the device name as arguments.
-func (pc *Paccap) LiveCapture(filter, device string) {
-	fmt.Println(banner)
+// packets recieved with the given expiration time. Takes BPF(filter),
+// the device name, snapshot length, promiscous (boolean) and timeout time as
+// arguments. As it recognizes the packets it keeps on pushing the packets into
+// a channel shared with the host process that is the client.
+func (pc *Paccap) LiveCapture(filter, device string, snapshotlen int32,
+	promiscuous bool, timeout time.Duration) {
 	logger.Info("[PacCap ] Capturing NIC to read the packets...")
-	fmt.Println()
-	packetSource, err := getPackets(filter, device)
+	packetSource, err := getPackets(filter, device, snapshotlen, promiscuous, timeout)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for packet := range packetSource.Packets() {
-		sip, dip := getIPAddresses(packet)
-		spn, dpn := getPortAddresses(packet)
-		logger.Infof("[PacCap ] Packet Captured! SOURCE %v:%v | DESTINATION %v:%v",
-			sip, spn, dip, dpn)
-		// observe cache hits
-		_, found := pc.ipcache.Get(sip)
-		if !found {
-			pc.ipcache.Set(sip, dpn)
-		} else if pc.lc {
-			logger.Infof("[PacCap ] Cache hit! The above packet is already in cache.")
+		sip, dip, err := getIPAddresses(packet)
+		if err != nil {
+			log.Errorf("[paccap ] %s", err.Error())
 		}
-		fmt.Println()
+		spn, dpn, err := getPortAddresses(packet)
+		if err != nil {
+			log.Errorf("[paccap ] %s", err.Error())
+		}
+
+		pkt := Packet{FromIP: sip, ToIP: dip, FromPort: spn, ToPort: dpn}
+		pc.PackChan <- pkt
+
+		// populating the cache
+		pc.IPCache.Set(pkt)
 	}
 }
 
@@ -140,10 +154,11 @@ func readPackets(filter, path string) (*gopacket.PacketSource, error) {
 }
 
 // getPackets returns the PacketSource for analysing the packets.
-func getPackets(filter, device string) (*gopacket.PacketSource, error) {
+func getPackets(filter, device string, snapshotlen int32,
+	promiscuous bool, timeout time.Duration) (*gopacket.PacketSource, error) {
 	handle, err := pcap.OpenLive(
 		device,
-		snapshot_len,
+		snapshotlen,
 		promiscuous,
 		timeout,
 	)
@@ -164,29 +179,31 @@ func getPackets(filter, device string) (*gopacket.PacketSource, error) {
 
 // getIPAddresses returns the IP addresses of source and
 // Destination
-func getIPAddresses(packet gopacket.Packet) (string, string) {
+func getIPAddresses(packet gopacket.Packet) (string, string, error) {
 	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv4)
-		return ip.SrcIP.String(), ip.DstIP.String()
+		return ip.SrcIP.String(), ip.DstIP.String(), nil
 	} else if ipLayer := packet.Layer(layers.LayerTypeIPv6); ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv6)
-		return ip.SrcIP.String(), ip.DstIP.String()
+		return ip.SrcIP.String(), ip.DstIP.String(), nil
 	}
-	log.Errorf("[PacCap ] Couldn't inspect Network Layer.")
-	return "nil", "nil"
+	// create a new error
+	err := errors.New("Couldn't inspect Network layer.")
+	return "", "", err
 }
 
 // getPortAddresses returns the Port addresses of source and
 // Destination
-func getPortAddresses(packet gopacket.Packet) (string, string) {
+func getPortAddresses(packet gopacket.Packet) (string, string, error) {
 	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 		tcp, _ := tcpLayer.(*layers.TCP)
-		return tcp.SrcPort.String(), tcp.DstPort.String()
+		return tcp.SrcPort.String(), tcp.DstPort.String(), nil
 	}
 	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
 		udp, _ := udpLayer.(*layers.UDP)
-		return udp.SrcPort.String(), udp.DstPort.String()
+		return udp.SrcPort.String(), udp.DstPort.String(), nil
 	}
-	log.Errorf("[PacCap ] Couldn't inspect Transport Layer.")
-	return "nil", "nil"
+	// create a new error
+	err := errors.New("Couldn't inspect Transport layer.")
+	return "", "", err
 }
